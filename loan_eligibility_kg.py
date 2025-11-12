8b588c51fe9dcccf270dc37f545f524f6d98a52f
Andres <61357716+andresbaldeon93@users.noreply.github.com>
2025-11-12 12:30:11 -0500

    preprocessing

--- loan_eligibility_kg.py
+++ loan_eligibility_kg.py
@@ -1,31 +1,68 @@
 
-import polars as pl
+import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn import preprocessing
 from sklearn.metrics import accuracy_score 
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import LabelEncoder
+
 
 
 url ="https://raw.githubusercontent.com/andresbaldeon93/Loan_Eligibility_kg/refs/heads/main/Loan%20Eligibility%20Prediction.csv"
 
-df = pl.read_csv(url,separator=",")
+df = pd.read_csv(url,sep=",")
 #print(df.describe())
 #print(df.columns)
+#print(df.dtypes)
+#df.info()
+#print(df.columns)
+#print(type(df))
 
-df = pl.DataFrame(df)
-#x = df['Gender', 'Married', 'Dependents', 'Education', 'Self_Employed', 'Applicant_Income', 'Coapplicant_Income', 'Loan_Amount', 'Loan_Amount_Term', 'Credit_History', 'Property_Area']
-#y = df['Loan_Status'] 
+#PREPROCESSING
+col_numericas_X = ['Applicant_Income','Coapplicant_Income','Loan_Amount','Loan_Amount_Term','Credit_History']
+col_categoricas_X = ['Gender','Married','Education','Self_Employed','Property_Area']
 
-X, y = df[:,:12],df['Loan_Status']
+preprocessor_X = ColumnTransformer(
+    transformers=[
+        ('num', StandardScaler(), col_numericas_X),
+        ('cat', OneHotEncoder(handle_unknown='ignore', sparse_output=False), col_categoricas_X)
+    ],
+    remainder='passthrough'
+)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=33)
+le = LabelEncoder()
 
-normalizer = preprocessing.Normalizer().fit(X_train)
+#CREACION DE MUESTRAS
+X , y = df.drop(columns=['Customer_ID','Loan_Status'],axis=1),df['Loan_Status']
 
-normalized_X = normalizer.transform(X_train)
-normalized_X_test = normalizer.transform(X_test)
+X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.2, random_state=33)
+
+y_train_index = y_train.index
+y_test_index = y_test.index
+
+X_train_processed = preprocessor_X.fit_transform(X_train)
+X_test_processed = preprocessor_X.transform(X_test)
+
+feature_names = preprocessor_X.get_feature_names_out()
+X_train_processed_df = pd.DataFrame(X_train_processed, columns=feature_names)
+X_test_processed_df = pd.DataFrame(X_test_processed, columns=feature_names)
+
+y_train_processed = le.fit_transform(y_train)
+y_test_processed = le.transform(y_test)
+
+y_train_processed_df = pd.DataFrame(y_train_processed,index=y_train_index,columns=['Loan_Status'])
+y_test_processed_df = pd.DataFrame(y_test_processed,index=y_test_index,columns=['Loan_Status'])
 
 
-print(normalized_X)
+pd.set_option('display.max_columns', None)
+
+#print(X_train_processed_df.head())
+print(y_test_processed_df.head())
+
+
+
+
+#print(normalized_X)
 #print(y_train)
 #print(X_test)
 #print(y_test)
